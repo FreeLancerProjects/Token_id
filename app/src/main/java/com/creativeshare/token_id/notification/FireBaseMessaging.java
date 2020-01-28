28fddd32d409ffc2093b2af379d55aec0f0d17f9
ARAB <ahmedmohamed23345@gmail.com>
2020-01-28 09:53:54 +0200

    ch

---
+++ app/src/main/java/com/creativeshare/token_id/notification/FireBaseMessaging.java
@@ -0,0 +1,56 @@
+package com.creativeshare.token_id.notification;
+
+import android.app.ActivityManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+import java.util.prefs.Preferences;
+
+public class FireBaseMessaging extends FirebaseMessagingService {
+
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Map<String,String> map = remoteMessage.getData();
+
+        for (String key:map.keySet())
+        {
+           Toast.makeText(this,"keys"+key+"value "+map.get(key),Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
